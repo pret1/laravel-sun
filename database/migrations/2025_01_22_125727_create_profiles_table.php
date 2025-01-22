8b0505dfdc5d08e0c61582912a464e3b45e9b278
konovsc <konovsc@gmail.com>
2025-01-22 16:26:01 +0300

    add Profile entity and migration
    php artisan make:model Profile -m
    php artisan migrate
    php artisan migrate:rollback --step=1

---
+++ database/migrations/2025_01_22_125727_create_profiles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone')->unique();
+            $table->string('address');
+            $table->enum('gender', ['male', 'female', 'other']);;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
