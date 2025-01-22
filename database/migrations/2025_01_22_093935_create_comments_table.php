8a57db14586ef601f42db8848624413a58bed6a5
konovsc <konovsc@gmail.com>
2025-01-22 12:51:21 +0300

    add Comment entity and migration
    php artisan make:model Comment -m
    php artisan migrate
    php artisan migrate:rollback --step=1

---
+++ database/migrations/2025_01_22_093935_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('post');
+            $table->text('content');
+            $table->string('author');
+            $table->boolean('status');
+            $table->string('parent');
+            $table->unsignedInteger('like');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
