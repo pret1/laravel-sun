9b31dd5036004689acb9a06d38a94fd25e832373
konovsc <konovsc@gmail.com>
2025-01-27 12:44:11 +0300

    Basic crud Post with route

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return $posts = Post::all();
+    }
+
+    public function store()
+    {
+         return Post::create([
+            'title' => 'Command title',
+            'content' => 'content command',
+            'author' => 'author command',
+            'is_published' => true,
+            'likes' => 1,
+            'image_path' => '/var/www/html/' . rand(0, 1000) . '/' . rand(0, 1000) .'/images/image.jpg',
+            'tag' => 'tag command',
+            'category' => 'category command',
+            'views' => 1,
+            'published_at' => '2025-01-23 16:38:27',
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        $post = Post::findOrFail($post);
+        dd($post);
+    }
+
+    public function update()
+    {
+        $data = [
+            'title' => 'update title',
+        ];
+        $post = Post::find(10);
+        $post->update($data);
+        return $post;
+    }
+
+    public function destroy()
+    {
+        $post = Post::find(10);
+        $post->delete();
+        return response([
+            'message' => 'delete success',
+        ], ResponseAlias::HTTP_OK);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts/index', [PostController::class, 'index']);
+Route::get('/posts/{post}/show', [PostController::class, 'show']);
+Route::get('/posts/store', [PostController::class, 'store']);
+Route::get('/posts/update', [PostController::class, 'update']);
+Route::get('/posts/destroy', [PostController::class, 'destroy']);
