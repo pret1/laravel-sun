b861d90ecf34ad32f96ba4d1f1b34cafbb99e533
konovsc <konovsc@gmail.com>
2025-01-29 11:11:26 +0300

    check list routs
    php artisan route:list --path=posts
    or
    php artisan route:list --path=api/posts

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'posts'], function () {
+    Route::get('/index', [PostController::class, 'index']);
+    Route::get('/{post}/show', [PostController::class, 'show']);
+    Route::get('/store', [PostController::class, 'store']);
+    Route::get('/{post}/update', [PostController::class, 'update']);
+    Route::get('/{post}/destroy', [PostController::class, 'destroy']);
+});
