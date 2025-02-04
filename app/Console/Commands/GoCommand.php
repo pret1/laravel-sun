8d0b87154f38e28911809a343678ec057304fdc3
konovsc <konovsc@gmail.com>
2025-02-04 12:43:50 +0300

    dumped all relations

--- app/Console/Commands/GoCommand.php
+++ app/Console/Commands/GoCommand.php
@@ -3,7 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
+use App\Models\Profile;
+use App\Models\Role;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Console\Command;
 
@@ -29,18 +33,60 @@ class GoCommand extends Command
     public function handle()
     {
         $action = $this->argument('action');
-        $post = Post::find(8);
+//        $post = Post::find(8);
 
         if ($action === 'r') {
-            $post = Post::first();
-            dd($post->likedProfiles);
-//            $user = User::first();
-//            dd($user);
 
-//            $post = Post::first();
+            //------COMMENTS---------
+//            $comment = Comment::first();
+//            $comment = Comment::find(3);
+//            dd($comment->likedProfiles?->toArray());
+//            dd($comment->post?->content);
+//            dd($comment->profile?->toArray());
+//            dd($comment->childrenComments?->toArray());
+//            dd($comment->parentComment?->toArray());
+            //------------------------------------------
+
+            //-------CATEGORIES----------
 //            $category = Category::first();
-//            dd($category->posts);
+//            $category = Category::find(2);
+//            dd($category->posts->toArray());
+            //---------------------------------
+
+            //--------POSTS------------------
+//            $post = Post::first();
+//            $post = Post::find(2);
+//            dd($post->category->toArray());
 //            dd($post->tags->toArray());
+//            dd($post->likedProfiles->toArray());
+//            dd($post->comments->toArray());
+            //-------------------------------
+
+            //---------Profiles----------
+//            $profile = Profile::first();
+//            $profile = Profile::find(2);
+//            dd($profile->user->toArray());
+//            dd($profile->likedPosts->toArray());
+//            dd($profile->comments->toArray());
+//            dd($profile->likedComments->toArray());
+            //----------------------------
+
+            //-----------Roles------------
+//            $role = Role::first();
+//            $role = Role::find(2);
+//            dd($role->users->toArray());
+            //-------------------------------
+
+            //----------Tags--------------
+//            $tag = Tag::first();
+//            dd($tag->posts->toArray());
+            //---------------------------
+
+            //----------Users--------------
+//            $user = User::first();
+//            dd($user->roles->toArray());
+//            dd($user->profile->toArray());
+            //-------------------------------
 
 //            $this->line(Post::find(8));
 //            dump(Post::all()->toArray());
