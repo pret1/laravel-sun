2b0c12fc3b197c1ecb598843bc0cd39d9371917b
konovsc <konovsc@gmail.com>
2025-03-20 12:12:54 +0300

    add some config for vue maybe unnecessary
    npm add -D vite-plugin-vue-devtools

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
     plugins: [
+        vueDevTools(),
+        createHtmlPlugin({}),
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
