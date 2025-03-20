a2c44d194b1c6d70e2b92e8b1f72c68abb19e598
konovsc <konovsc@gmail.com>
2025-03-20 11:16:00 +0300

    that rout need for use build-in login logout

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ Route::get('/', function () {
     ]);
 });
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
