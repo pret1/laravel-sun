1985ba0ee0e8b750ea64bb91b2301bf1f01cc75b
konovsc <konovsc@gmail.com>
2025-03-27 09:04:53 +0300

    added resource for PermissionController

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -8,17 +8,19 @@ use Illuminate\Http\Request;
 use Inertia\Response;
 use Inertia\ResponseFactory;
 use App\Http\Requests\Admin\Permission\StoreRequest;
+use App\Http\Resources\Permission\PermissionResource;
 
 class PermissionController extends Controller
 {
     public function index(): Response|ResponseFactory
     {
-        $permissions = Permission::all();
+        $permissions = PermissionResource::collection(Permission::all())->resolve();
         return inertia('Admin/Permission/Index', compact('permissions'));
     }
 
     public function show(Permission $permission): Response
     {
+        $permission = PermissionResource::make($permission)->resolve();
         return inertia('Admin/Permission/Show', compact('permission'));
     }
 
@@ -27,9 +29,10 @@ class PermissionController extends Controller
         return inertia('Admin/Permission/Create');
     }
 
-    public function store(StoreRequest $request): Response
+    public function store(StoreRequest $request): array
     {
         $data = $request->validated();
-        return Permission::create($data);
+        $permission = Permission::create($data);
+        return PermissionResource::make($permission)->resolve();
     }
 }
