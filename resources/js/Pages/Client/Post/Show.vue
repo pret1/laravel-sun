8fc20b3d06ffc91d72c32d2c27b69718ffc2fcc8
konovsc <konovsc@gmail.com>
2025-04-21 17:25:36 +0300

    number of likes for comments and child comments displays correctly

--- resources/js/Pages/Client/Post/Show.vue
+++ resources/js/Pages/Client/Post/Show.vue
@@ -47,13 +47,13 @@
                             <h3>Child Comments</h3>
                         </div>
                         <div>
-                            <div v-for="commentItem in childComments" class="mb-4 pb-4 border-b border-gray-200">
-                                <p>{{ commentItem.content }}</p>
+                            <div v-for="commentItemChild in childComments" class="mb-4 pb-4 border-b border-gray-200">
+                                <p>{{ commentItemChild.content }}</p>
                                 <span class="text-sm text-gray-500">{{ commentItem.published_at }}</span>
                                 <div class="flex justify-end">
-                                    <span>{{ commentItem.likes }}</span>
-                                    <svg @click="toggleLikeComment(commentItem.id)" xmlns="http://www.w3.org/2000/svg"
-                                         :fill="commentItem.is_liked ? '#000' : 'none'" viewBox="0 0 24 24" stroke-width="1.5"
+                                    <span>{{ commentItemChild.likes }}</span>
+                                    <svg @click="toggleLikeComment(commentItemChild.id, commentItem)" xmlns="http://www.w3.org/2000/svg"
+                                         :fill="commentItemChild.is_liked ? '#000' : 'none'" viewBox="0 0 24 24" stroke-width="1.5"
                                          stroke="currentColor" class="cursor-pointer size-6">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"/>
@@ -131,12 +131,15 @@ export default {
                 })
         },
 
-        toggleLikeComment(commentId) {
+        toggleLikeComment(commentId, parentComment) {
             axios.post(route('client.comment.like.toggle', commentId))
                 .then(res => {
-                    console.log(res)
-                    // this.comment.is_liked = res.data.is_liked
                     this.getComments()
+
+                    if(parentComment) {
+                        this.replyingTo = parentComment.id
+                        this.getChildComments(parentComment)
+                    }
                 })
         },
 
