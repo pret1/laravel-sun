612466c1810c7edaf3252b31359be0237223dccc
konovsc <konovsc@gmail.com>
2025-04-23 15:33:49 +0300

    added Like component with read only

--- resources/js/Components/Like/Like.vue
+++ resources/js/Components/Like/Like.vue
@@ -2,9 +2,16 @@
     <div>
         <div class="flex justify-end">
             <span>{{ item.likes}}</span>
-            <svg @click="toggleLike(item.id)" xmlns="http://www.w3.org/2000/svg"
+            <svg v-if="!readonly" @click="toggleLike(item.id)" xmlns="http://www.w3.org/2000/svg"
                  :fill="item.is_liked ? '#000' : 'none'" viewBox="0 0 24 24" stroke-width="1.5"
                  stroke="currentColor" class="cursor-pointer size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"/>
+            </svg>
+            <svg v-else
+                 xmlns="http://www.w3.org/2000/svg"
+                 :fill="item.is_liked ? '#000' : 'none'" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
                       d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"/>
             </svg>
@@ -21,7 +28,8 @@ export default {
     props: {
         item: Object,
         routeName: String,
-        onRefresh: Function
+        onRefresh: Function,
+        readonly: Boolean
     },
 
     methods: {
