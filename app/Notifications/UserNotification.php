d3f32d37aac4f7e8e2b10f6a3b5576475f1d22c1
konovsc <konovsc@gmail.com>
2025-05-20 16:46:24 +0300

    added database notifications
    php artisan make:notifications-table
    php artisan migrate
    php artisan make:notification UserNotification

--- app/Notifications/UserNotification.php
+++ app/Notifications/UserNotification.php
@@ -26,7 +26,7 @@ class UserNotification extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['database'];
     }
 
     /**
@@ -48,7 +48,7 @@ class UserNotification extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            'message' => 'some notification message',
         ];
     }
 }
