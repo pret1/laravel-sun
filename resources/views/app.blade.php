397670e3a2e58c8a8d074422d270abafc1782923
konovsc <konovsc@gmail.com>
2025-06-19 17:08:45 +0300

    try fixed CSRF problem

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
