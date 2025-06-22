6e69ee2c8b4e3b7b7df09579f0b59999f40e0bfd
konovsc <konovsc@gmail.com>
2025-06-22 09:48:46 +0300

    try fixed CSRF problem exclude login (CustomVerifyCsrfToken)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->app['router']->pushMiddlewareToGroup('web', CustomVerifyCsrfToken::class);
+//        $this->app['router']->pushMiddlewareToGroup('web', CustomVerifyCsrfToken::class);
 
         Model::unguard();
         Vite::prefetch(concurrency: 3);
